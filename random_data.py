0e39d7030d350bd6c02a19bd7abd52b1b748f525
analyst-triad <saadfarooq404@yahoo.com>
2023-12-09 18:07:01 +0500

    added appending python files

--- random_data.py
+++ random_data.py
@@ -6,7 +6,6 @@ import numpy as np
 # Set seed for reproducibility
 np.random.seed(42)
 
-
 # Function to generate dummy data and append to existing CSV
 def generate_and_append_data(file_path, num_machines=5, num_sensors=3, freq='H'):
     try:
@@ -58,13 +57,11 @@ def generate_dummy_data(start_date, end_date, num_machines=5, num_sensors=3, fre
 if __name__ == "__main__":
     data_file_path = 'dummy_sensor_data.csv'
     # Define date range for dummy data
-start_date = datetime(2023, 1, 1)
-end_date = datetime(2023, 1, 10)
+    start_date = datetime(2023, 1, 1)
+    end_date = datetime(2023, 1, 10)
 
-# Generate dummy data
-dummy_data = generate_dummy_data(start_date, end_date, num_machines=5, num_sensors=3)
+    # Generate dummy data
+    dummy_data = generate_dummy_data(start_date, end_date, num_machines=5, num_sensors=3)
 
-# Save dummy data to CSV file
-dummy_data.to_csv('dummy_sensor_data.csv', index=False)
-
-generate_and_append_data(data_file_path)
+    # Save dummy data to CSV file
+    dummy_data.to_csv('dummy_sensor_data.csv', index=False)
