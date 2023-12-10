1592bb521f248107d757029a4e032c35e6689915
analyst-triad <saadfarooq404@yahoo.com>
2023-12-10 20:02:47 +0500

    updated retraining logic

--- retrain.py
+++ retrain.py
@@ -19,18 +19,45 @@ y = data['Reading']
 # Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42, shuffle=False)
 
-loaded_model.fit(X_train, y_train)
+# Check if metrics difference exceeds the threshold
+DIFF_THRESHOLD = 0.1
+previous_metrics_file = 'current_metrics.json'
+current_metric_file = 'new_metrics.json'
 
-# Make predictions on the test set
-y_pred = loaded_model.predict(X_test)
+try:
+    with open(previous_metrics_file, 'r') as json_file:
+        previous_mse_dict = json.load(json_file)
+        previous_mse = previous_mse_dict.get('mse', 0)
+except (FileNotFoundError, json.JSONDecodeError):
+    previous_mse = 0
+    
+    
+try:
+    with open(current_metric_file, 'r') as json_file:
+        current_mse_dict = json.load(json_file)
+        current_mse = current_mse_dict.get('mse', 0)
+except (FileNotFoundError, json.JSONDecodeError):
+    current_mse = 0
 
-# Evaluate the new model
-mse = mean_squared_error(y_test, y_pred)
-print(f"Mean Squared Error on the test set: {mse}")
+metrics_difference = abs(current_mse - previous_mse)
 
-# Log MSE to a JSON file
-mse_dict = {'mse': mse}
-with open('current_metrics.json', 'w') as json_file:
-    json.dump(mse_dict, json_file)
+if metrics_difference > DIFF_THRESHOLD:
+    print("Metrics difference exceeds the threshold. Retraining needed.")
+    loaded_model.fit(X_train, y_train)
+    # Make predictions on the test set
+    y_pred = loaded_model.predict(X_test)
+    # Evaluate the new model
+    mse = mean_squared_error(y_test, y_pred)
+    print(f"Mean Squared Error on the test set: {mse}")
+    # Log MSE to a JSON file
+    mse_dict = {'mse': mse}
+    with open('current_metrics.json', 'w') as json_file:
+        json.dump(mse_dict, json_file)
+else:
+    
+    print("Metrics difference is within the threshold. No retraining needed.")
+
+
+
 
 
