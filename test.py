74e5be3e118b3435a47709a16ec5d515cbbe5988
analyst-triad <saadfarooq404@yahoo.com>
2023-12-10 19:23:02 +0500

    modified test.py to take command line arguments

--- test.py
+++ test.py
@@ -4,27 +4,40 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 import json
+import sys
 
-# Load data
-data = pd.read_csv("preprocess_data.csv")
+def train_and_evaluate(metric_file):
+    # Load data
+    data = pd.read_csv("preprocess_data.csv")
 
-# Define features (X) and target variable (y)
-X = data[['Hour', 'Machine_ID', 'Sensor_ID']]
-y = data['Reading']
+    # Define features (X) and target variable (y)
+    X = data[['Hour', 'Machine_ID', 'Sensor_ID']]
+    y = data['Reading']
 
-# Split the dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, shuffle=False)
+    # Split the dataset into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, shuffle=False)
 
-# Load the model using MLflow
-shared_model_path = "best_model/"
-loaded_model = mlflow.sklearn.load_model(shared_model_path)
+    # Load the model using MLflow (replace "best_model/" with the actual path)
+    loaded_model = mlflow.sklearn.load_model("best_model/")
 
-# Calculate Mean Squared Error
-y_pred = loaded_model.predict(X)
-mse = mean_squared_error(y, y_pred)
-print(f'Mean Squared Error: {mse}')
+    # Calculate Mean Squared Error
+    y_pred = loaded_model.predict(X_test)
+    mse = mean_squared_error(y_test, y_pred)
+    print(f'Mean Squared Error: {mse}')
 
-# Log MSE to a JSON file
-mse_dict = {'mse': mse}
-with open('current_metrics.json', 'w') as json_file:
-    json.dump(mse_dict, json_file)
+    # Log MSE to the specified JSON file
+    mse_dict = {'mse': mse}
+    with open(metric_file, 'w') as json_file:
+        json.dump(mse_dict, json_file)
+
+if __name__ == "__main__":
+    # Check if the correct number of command line arguments is provided
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <metric_file>")
+        sys.exit(1)
+
+    # Get the metric file name from the command line arguments
+    metric_file = sys.argv[1]
+
+    # Call the train_and_evaluate function with the provided metric file name
+    train_and_evaluate(metric_file)
